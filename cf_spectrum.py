dffcf41cd6906ed52a24347bd0bfdf0ef0e61630
arno <arnode125@gmail.com>
2019-04-16 22:41:39 +0800

    初始化

---
+++ cf_spectrum.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+#-_- coding: utf-8 -_-
+
+import requests, json, sys
+from config          import *
+from cf_spectrum_api import cfSpectrumApi
+#禁用安全请求警告
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+#cf接口，账户参数
+cf_url = "https://api.cloudflare.com/client/v4/zones"
+email  = "L5yirencp@gmail.com"
+key    = "5321b5c5022b60107a9e5ad295539c58ee551"
+
+zone_id = "689438eb0e73fd72cdbf01067c22575d" #bbqp5566.com zone_id
+
+# #requests参数
+# headers = {'X-Auth-Email': email, 'X-Auth-Key': key, 'Content-Type': 'application/json'}
+
+#给cf 后台域名添加白名单
+for customer in manage:
+    if customer == "1717cp":
+        info = manage[customer]
+        csa = cfSpectrumApi(info['email'], info['key'])
+        for ip in info['whitelist']:
+            result = csa.CreateIpfirewall(info['zone_id'], ip, mode='whitelist', notes="后台白名单")
+            print (result)
+            # break
+
+sys.exit()
+
+#获取api 接口
+csa = cfSpectrumApi(email, key)
+
+#获取指定域名的applist
+appList = csa.GetSpectList(zone_id)['result']
+if not appList: sys.exit(1)
+
+#block 非法IP
+# print (csa.CreateIpfirewall(zone_id, "192.168.100.2", notes="非法请求tcp"))
+# sys.exit(0)
+
+for app in appList:
+    if app['protocol'] == "tcp/8800":
+        print (app['id'], app['dns']['name'], app['protocol'], app['origin_direct'], app['ip_firewall'], app['proxy_protocol'])
+        # if app['dns']['name'] == "lgrm2.bbqp5566.com":
+        #     app['origin_direct'] = ['tcp://8.8.8.8:8800']
+        #     print (csa.UpdateApp(zone_id, app))
+    continue
+
+#     if app['dns']['name'] == "lgrm2.bbqp5566.com":
+#         zone_id = "ea898577a1186e8c8a5d3e7fb7ab35d3"
+#         app['dns']['name'] = "lgrm.bbqp0555.com"
+#         app['origin_direct'] = [app['origin_direct'][0].replace("47.244.106.52", "59.188.233.197")]
+#         print (csa.createApp(zone_id, app))
+#         continue
+    if app['protocol'] == "tcp/8800" and app['dns']['name'] in ["lgrm3.bbqp5566.com", "lgrm4.bbqp5566.com"]:
+        # app['origin_direct'] = ['tcp://8.8.8.8:8800']
+        app['origin_direct'] = [
+                'tcp://47.244.106.52:8800',
+                'tcp://216.118.228.180:8800',
+                'tcp://216.118.245.220:8800',
+                'tcp://180.178.32.100:8800',
+                'tcp://52.128.245.124:8800',
+                'tcp://182.16.117.180:8800',
+                ]
+        print (csa.UpdateApp(zone_id, app))
+        continue
+
+    if app['dns']['name'] not in [""]:
+        if app['protocol'] == "tcp/8800":
+            print (app['id'], app['dns']['name'], app['protocol'], app['origin_direct'], app['ip_firewall'], app['proxy_protocol'])
+            # print (app)
+            # app['origin_direct'] = ['tcp://8.8.8.8:8800']
+            # updateApp(app)
+    # updateApp(app)
+    # continue
+    # print (app)
+    # break
+    # print (app['id'], app['dns']['name'], app['protocol'], app['origin_direct'], app['ip_firewall'], app['proxy_protocol'])
+    
---
+++ cf_spectrum_api.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+#-_- coding: utf-8 -_-
+
+import requests, json, sys
+from config import cf_accounts, timeout
+
+#禁用安全请求警告
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class cfSpectrumApi(object):
+    def __init__(self, email, key, app={}, timeout=5):
+        self.__url   = "https://api.cloudflare.com/client/v4"
+        self.__email = email
+        self.__key   = key
+        self.__app   = app
+        self.__timeout = timeout
+        self.__headers = {
+            'X-Auth-Email': self.__email, 
+            'X-Auth-Key':   self.__key, 
+            'Content-Type': 'application/json'
+        }
+
+    def GetSpectList(self, zone_id):
+        '''
+            获取指定 zone_id 的app 列表
+        '''
+        url = self.__url + "/zones/%s" %zone_id + "/spectrum/apps"
+        try:
+            ret = requests.get(url, headers=self.__headers, verify=False, timeout=self.__timeout)
+        except Exception as e:
+            return {"success": False, "result": None}
+        else:
+            return ret.json()
+
+    def UpdateApp(self, zone_id, app):
+        '''
+            更新指定 zone_id 的app，app中必须包含id [97fc9e6bceb1418b9e1ea8550d23cbc6]，以及
+            {'protocol': "tcp/8800",
+            'dns': {'type': 'CNAME', 'name': 'lgrm2.bbqp5566.com'},
+            'origin_direct': ['tcp://8.8.8.8:8800'],
+            'ip_firewall': True,
+            'proxy_protocol': True,}
+        '''
+        url  = self.__url + "/zones/%s" %zone_id + "/spectrum/apps/%s" %app['id']
+        data = {
+            'protocol': app['protocol'],
+            'dns': app['dns'],
+            'origin_direct': app['origin_direct'],
+            'ip_firewall': app['ip_firewall'],
+            'proxy_protocol': app['proxy_protocol'],
+        }
+        try:
+            ret  = requests.put(url, headers=self.__headers, data=json.dumps(data), verify=False, timeout=self.__timeout)
+        except Exception as e:
+            return {"success": False, "result": None}
+        else:
+            return ret.json()
+
+    def createApp(self, zone_id, app):
+        '''
+            新增指定 zone_id 的app，app中必须包含id [97fc9e6bceb1418b9e1ea8550d23cbc6]，以及
+            {'protocol': "tcp/8800",
+            'dns': {'type': 'CNAME', 'name': 'lgrm2.bbqp5566.com'},
+            'origin_direct': ['tcp://8.8.8.8:8800'],
+            'ip_firewall': True,
+            'proxy_protocol': True,}
+        '''
+        url  = self.__url + "/zones/%s" %zone_id + "/spectrum/apps"
+        data = {
+            'protocol': app['protocol'],
+            'dns': app['dns'],
+            'origin_direct': app['origin_direct'],
+            'ip_firewall': True,
+            'proxy_protocol': True,
+        }
+        try:
+            ret  = requests.post(url, headers=self.__headers, data=json.dumps(data), verify=False, timeout=self.__timeout)
+        except Exception as e:
+            return {"success": False, "result": None}
+        else:
+            return ret.json()
+
+    def CreateIpfirewall(self, zone_id, ip, mode="block", notes="新增规则"):
+        '''
+            添加ip 防火墙规则，主要应该是block IP
+            mode: block, challenge, whitelist, js_challenge
+        '''
+        url = self.__url + "/zones/%s" %zone_id + "/firewall/access_rules/rules"
+        data = {
+            "mode": mode,
+            "configuration": {
+                    "target": "ip",
+                    "value": ip,
+                },
+            "notes": "%s: %s" %(mode, notes),
+        }
+        try:
+            ret  = requests.post(url, headers=self.__headers, data=json.dumps(data), verify=False, timeout=self.__timeout)
+        except Exception as e:
+            print (str(e))
+            return {"success": False, "result": None}
+        else:
+            return ret.json()
+
+if __name__ == "__main__":
+    print ("no more")
