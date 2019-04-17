fed3d6a36e62e1c9bab9adbdba0f64ca3e3dc392
xinpay <xinpay1915@gmail.com>
2019-04-17 11:26:27 +0800

    update lgrm2.appifc.com

--- cf_spectrum.py
+++ cf_spectrum.py
@@ -43,22 +43,22 @@ if not appList: sys.exit(1)
 # sys.exit(0)
 
 for app in appList:
-    print (app['id'], app['dns']['name'], app['protocol'], app['origin_direct'], app['ip_firewall'], app['proxy_protocol'])
-    continue
-    sys.exit()
-    if app['protocol'] == "tcp/8800":
-        print (app['id'], app['dns']['name'], app['protocol'], app['origin_direct'], app['ip_firewall'], app['proxy_protocol'])
-        # if app['dns']['name'] == "lgrm2.bbqp5566.com":
-        #     app['origin_direct'] = ['tcp://8.8.8.8:8800']
-        #     print (csa.UpdateApp(zone_id, app))
-    continue
+    # print (app['id'], app['dns']['name'], app['protocol'], app['origin_direct'], app['ip_firewall'], app['proxy_protocol'])
+    # continue
+    # sys.exit()
+    # if app['protocol'] == "tcp/8800":
+    #     print (app['id'], app['dns']['name'], app['protocol'], app['origin_direct'], app['ip_firewall'], app['proxy_protocol'])
+    #     # if app['dns']['name'] == "lgrm2.appifc.com":
+    #     #     app['origin_direct'] = ['tcp://8.8.8.8:8800']
+    #     #     print (csa.UpdateApp(zone_id, app))
+    # continue
 
-#     if app['dns']['name'] == "lgrm2.bbqp5566.com":
-#         zone_id = "ea898577a1186e8c8a5d3e7fb7ab35d3"
-#         app['dns']['name'] = "lgrm.bbqp0555.com"
-#         app['origin_direct'] = [app['origin_direct'][0].replace("47.244.106.52", "59.188.233.197")]
-#         print (csa.createApp(zone_id, app))
-#         continue
+    if app['dns']['name'] == "lgrm2.appifc.com":
+        # zone_id = "ea898577a1186e8c8a5d3e7fb7ab35d3"
+        # app['dns']['name'] = "lgrm.bbqp0555.com"
+        app['origin_direct'] = [app['origin_direct'][0].replace("47.75.141.163", "52.128.245.125")]
+        print (csa.UpdateApp(zone_id, app))
+        continue
     if app['protocol'] == "tcp/8800" and app['dns']['name'] in ["lgrm3.bbqp5566.com", "lgrm4.bbqp5566.com"]:
         # app['origin_direct'] = ['tcp://8.8.8.8:8800']
         app['origin_direct'] = [
